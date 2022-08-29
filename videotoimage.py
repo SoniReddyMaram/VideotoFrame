4b1ecc8602c55e6059cfaa756912da3e19a7d434
SoniReddyMaram <103919843+SoniReddyMaram@users.noreply.github.com>
2022-08-29 14:35:55 +0800

    Add files via upload

---
+++ videotoimage.py
@@ -0,0 +1,47 @@
+import sys
+import cv2
+import os
+from glob import glob
+
+def extractFrames(pathIn, pathOut):
+    
+    if not os.path.exists(pathOut):
+        os.mkdir(pathOut)
+        
+    
+ 
+    video_list = glob(pathIn + '/*.avi')
+    count=1
+    
+    for video in video_list:
+        print("Process the video",video)
+        
+        cap = cv2.VideoCapture(video)
+        if not cap.isOpened():
+            print('Cant open video {}'.format(video))
+            sys.exit(0)
+        
+        skip_frame=0
+        while (cap.isOpened()):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            fps = round(cap.get(5))
+            skip_frame=skip_frame+fps*60
+            if ret == True:
+                print('Read %d frame: ' % count, ret)
+                cv2.imwrite(os.path.join(pathOut, "frame{:d}.jpg".format(count)), frame)  
+                count= count+1
+                cap.set(cv2.CAP_PROP_POS_FRAMES,skip_frame)
+            else:
+                print("Done")
+                break
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
+    
+def main():
+    extractFrames("H:\OD\camera2", 'C2')
+    # extractFrames("D:\\Fisherman\\2022-2-08\\Camera3", 'c2')
+    # extractFrames("D:\\Fisherman\\2022-2-08\\Camera3", 'c3')
+if __name__=="__main__":
+    main()
